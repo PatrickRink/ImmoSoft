e72704d37f2aa65a43a442f6b08f3cbcdb5ce851
PatrickRink <patrickrink@gmx.de>
2025-10-16 15:43:17 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,15 @@
+import React from "react";
+import PropertyList from "./components/PropertyList";
+import TenantList from "./components/TenantList";
+import DocumentUpload from "./components/DocumentUpload";
 
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>🏠 Immobilienverwaltung</h1>
+      <PropertyList />
+      <TenantList />
+      <DocumentUpload />
+    </div>
+  );
+}
